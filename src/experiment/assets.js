c88bdc8234708e2e48a67bb5394bd276f548d0c6
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-11-05 17:20:11 +0100

    init

---
+++ src/experiment/assets.js
@@ -0,0 +1,10 @@
+export const assets = [
+  {
+    name: 'base',
+    data: {},
+    items:
+      [
+        {name: 'lennaTexture', source: '/assets/lenna.png', type: 'texture'},
+      ],
+  },
+];
---
+++ src/script.js
@@ -0,0 +1,8 @@
+import './style.css';
+import { experiment } from './experiment/experiment.js';
+
+experiment.init ({
+  targetElement: document.querySelector ('.experience'),
+});
+
+console.log (experiment);
